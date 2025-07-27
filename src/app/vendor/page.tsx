8dd52a4f763755ceb1ef845eacb686ef958b7a0f
Niranjanan T <niranjanant7@gmail.com>
2025-07-27 21:02:49 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <X className="h-4

--- src/app/vendor/page.tsx
+++ src/app/vendor/page.tsx
@@ -177,13 +177,13 @@ export default function VendorPage() {
                                     </Card>
                                 </DialogTrigger>
                                 <DialogContent className="p-0 bg-transparent border-none shadow-none flex items-center justify-center">
-                                    <div className="w-[375px] h-[667px] scale-[0.8] rounded-2xl overflow-hidden border-4 border-foreground bg-background">
+                                    <div className="relative w-[375px] h-[667px] scale-[0.8] rounded-2xl overflow-hidden border-4 border-foreground bg-background">
                                       <TemplatePreview templateName={template.name} />
+                                       <DialogClose className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary text-white bg-black/50">
+                                            <X className="h-4 w-4" />
+                                            <span className="sr-only">Close</span>
+                                        </DialogClose>
                                     </div>
-                                    <DialogClose className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary text-white bg-black/50">
-                                        <X className="h-4 w-4" />
-                                        <span className="sr-only">Close</span>
-                                    </DialogClose>
                                 </DialogContent>
                             </Dialog>
                         ))}
