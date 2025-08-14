149aa648591efe08c8c7ff6972b99e4b6f4d2632
Niranjanan T <niranjanant7@gmail.com>
2025-08-14 20:13:57 +0000

    is 404 - Page Not Found Sorry, we couldn't find the page you were lookin

---
+++ src/app/vendor/both/(dashboard)/corporate-orders/new/page.tsx
@@ -0,0 +1,18 @@
+
+"use client";
+
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+
+export default function NewCorporateOrderPage() {
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>New Corporate Order</CardTitle>
+                <CardDescription>This page is under construction.</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <p>Functionality for manually creating corporate orders will be implemented here.</p>
+            </CardContent>
+        </Card>
+    )
+}
