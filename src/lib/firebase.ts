84c0b0cb71093ac6aa121a66632abeb0d14dca52
Niranjanan T <niranjanant7@gmail.com>
2025-08-15 17:47:01 +0000

    I clicked the "Seed Database" and waited for a few minuted but still una

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,8 +1,9 @@
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAnalytics, isSupported } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 // For more information on how to get this object, see the link below:
@@ -18,12 +19,13 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 // Initialize Analytics if it is supported
 const analytics = isSupported().then(yes => yes ? getAnalytics(app) : null);
 
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
+const storage = getStorage(app);
 
-export { app, analytics, db };
+export { app, analytics, db, storage };
