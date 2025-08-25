d81b798961cb7afb48036d377dba2500a7817f7b
Sangeeth21 <vishnusangeeth21@gmail.com>
2025-08-26 02:16:49 +0530

    deploy-3

--- src/app/vendor/personal/(dashboard)/messages/page.tsx
+++ src/app/vendor/personal/(dashboard)/messages/page.tsx
@@ -6,7 +6,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { Search, Send, MessageSquare, AlertTriangle, Check, EyeOff, Eye, CheckCheck } from "lucide-react";
+import { Search, Send, MessageSquare, AlertTriangle, Check, EyeOff, Eye, CheckCheck, FileIcon, Download, ImageIcon, Paperclip, X } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { useToast } from "@/hooks/use-toast";
 import Image from "next/image";
